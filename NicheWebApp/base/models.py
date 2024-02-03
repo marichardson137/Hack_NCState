52ac3d7c604d384d019e3933531e1f7fb413e949
maricha6 <maricha6@ncsu.edu>
2024-02-03 16:43:07 -0500

    [Database] Added Tag and Card models to DB

--- NicheWebApp/base/models.py
+++ NicheWebApp/base/models.py
@@ -2,3 +2,25 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Card(models.Model):
+    title = models.CharField(max_length=255, default=None)
+    image = models.ImageField(upload_to='images/', default=None)
+    tags = models.ManyToManyField(Tag, related_name="tags", blank=True)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
+
+    def __str__(self):
+        return self.title
